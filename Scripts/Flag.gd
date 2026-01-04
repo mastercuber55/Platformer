b8452a5a2df21dd49883e28d14264ad029f2578f
Cube Nerd <mastercuber55@gmail.com>
2026-01-04 17:49:10 +0530

    Improved some things and enhanced DiscordRPC

--- Scripts/Flag.gd
+++ Scripts/Flag.gd
@@ -1,4 +1,6 @@
 extends Node2D
 
+@export_file("*.tscn") var nextLevel: String
+
 func _on_area_entered(body: Node2D) -> void:
-	Global.health = 0
+	get_tree().change_scene_to_file(nextLevel)
--- Scripts/KillZone.gd
+++ Scripts/KillZone.gd
@@ -2,3 +2,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:	
 	Global.health = 0
+	# TODO: make it so that when the player falls below the ground, it wraps to the top of the map and falls down to the last checkpoint like human fall flat.
